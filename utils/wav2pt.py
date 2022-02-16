455ca25206d352df9ee26ef8e4d9cc7f31483e3b
xvdp <xvdpahlen@gmail.com>
2022-02-16 13:56:12 -0800

    lining last checkin: builtin function dir should not be overwritten

--- utils/wav2pt.py
+++ utils/wav2pt.py
@@ -1,27 +1,24 @@
+import multiprocessing as mp
+import os
+from glob import glob
 import torch
 import librosa as rosa
 from omegaconf import OmegaConf as OC
-import os
-from glob import glob
 from tqdm import tqdm
-import multiprocessing as mp
 
 def wav2pt(wav):
-    y,_ = rosa.load(wav, sr = hparams.audio.sr, mono = True)
+    y,_ = rosa.load(wav, sr=hparams.audio.sr, mono=True)
     y,_ = rosa.effects.trim(y, top_db=15)
     pt_name = os.path.splitext(wav)[0]+'.pt'
     pt = torch.tensor(y)
-    torch.save(pt ,pt_name)
-    del y, pt 
+    torch.save(pt, pt_name)
+    del y, pt
     return
 
 if __name__=='__main__':
     hparams = OC.load('hparameter.yaml')
-    dir = hparams.data.dir
-    wavs = glob(os.path.join(dir, '*/*.flac'))
+    wavs = glob(os.path.join(hparams.data.dir, '*/*.flac'))
     pool = mp.Pool(processes = hparams.train.num_workers)
     with tqdm(total = len(wavs)) as pbar:
         for _ in tqdm(pool.imap_unordered(wav2pt, wavs)):
             pbar.update()
-
-            
